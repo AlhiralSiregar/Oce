9de88329def596a9a77a9713a5d7278192e910a5
AlhiralSiregar <hiral_kirakun@yahoo.com>
2015-04-10 10:34:50 +0700

    Prima
    prima java sumatera kalimantan sulawesi papua

---
+++ no_2/src/no_2/No_2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package no_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class No_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner in = new Scanner (System.in);   
+    int oce1,oce2;
+    System.out.println("Input");
+    System.out.print("Awal    : ");
+    oce1=in.nextInt();
+    System.out.print("Akhir   : ");
+    oce2=in.nextInt();
+    System.out.println("Output");
+    System.out.print("Deret bilangan prima antara "+oce1+" dan "+oce2+" adalah \n");
+    int num=oce1;
+    while (num <= oce2) {
+        int tmp = 0;
+        for (int i=2; i<num; i++) {
+            if (num%i==0){
+                tmp=1;
+            }
+        }
+        if (tmp != 1){
+            System.out.print(num);
+        }
+        num++;
+    }
+
+} // TODO code application logic here
+    }
+    
+
